3e9b27d66c349293943300a354dbe0405409275d
Erilyth <vishal.batchu@students.iiit.ac.in>
2016-06-16 17:12:21 +0530

    Add another convolution layer and train longer

--- NumberConvNet/convnets.lua
+++ NumberConvNet/convnets.lua
@@ -9,18 +9,23 @@
 require "nn"
 require "image"
 
--- Test neural network which calculates the sum of the inputs
--- Sample run on 1000 tests with a training rate of 0.01
+-- Test neural network which determines digit from handwritten images of digits
+-- Sample run on 1000 tests with a training rate of 1
 
 -- Parametrs for SpatialConvolution = (inputlayers, outputlayers, kernel_width, kernel_height, x_stride, y_stride, x_padding, y_padding)
 -- Parametrs for SpatialMaxPooling = (width, height, x_stride, y_stride)
 cnn = nn.Sequential()
--- Input = 3*8*8 image, Output = 2*8*8 since the padding of 1 will ensure the same size remains
-cnn:add(nn.SpatialConvolution(3,1,3,3,1,1,1,1))
+-- Input = 3*8*8 image, Output = 4*8*8 since the padding of 1 will ensure the same size remains
+cnn:add(nn.SpatialConvolution(3,4,3,3,1,1,1,1))
 cnn:add(nn.ReLU())
 cnn:add(nn.Sigmoid())
--- Reduce the size from 2*8*8 to 2*4*4
+-- Input = 4*8*8 image, Output = 1*8*8 since the padding of 1 will ensure the same size remains
+cnn:add(nn.SpatialConvolution(4,1,3,3,1,1,1,1))
+cnn:add(nn.ReLU())
+cnn:add(nn.Sigmoid())
+-- Reduce the size from 1*8*8 to 1*4*4 (Not done right now as the image is already small)
 -- cnn:add(nn.SpatialMaxPooling(2,2,2,2))
+-- Reshape from a 3d array to a 1d array
 cnn:add(nn.Reshape(1*8*8))
 -- Apply a completely connected neural network to get 10 outputs
 cnn:add(nn.Linear(1*8*8,10))
@@ -28,7 +33,7 @@ cnn:add(nn.Sigmoid())
 
 criterion = nn.MSECriterion()
 
-for p=1,100 do
+for p=1,500 do
 	for i=0,9 do
 	    for j=1,15 do
 	        local output = torch.Tensor(10);
@@ -46,12 +51,14 @@ for p=1,100 do
 	        -- Accumulate gradients and back propogate
 	        cnn:backward(img, criterion:backward(cnn.output, output))
 	        -- Update with a learning rate
-	        cnn:updateParameters(0.5)
+	        cnn:updateParameters(0.1)
 	    end
 	end
 end
 
-
+correct_matches = 0
+accuracy = 0
+total = 0
 
 -- Test the network
 for q=0,9 do
@@ -59,5 +66,23 @@ for q=0,9 do
     print(imgfile)
     local img = image.load(imgfile,3)
     img:resize(3,8,8)
-    print(cnn:forward(img))
+    results = cnn:forward(img)[1]
+    best_result = torch.max(results)
+    local cur_acc = results[q+1]
+    total = 0
+    for p=1,10 do
+    	total = total + results[p]
+    	if results[p] == best_result then
+    		answer = p-1
+    	end
+    end
+    print(answer)
+    accuracy = accuracy + cur_acc / total
+    if answer == q then
+    	correct_matches = correct_matches + 1
+    end
 end
+
+print('Final Results:')
+print('Correct Matches = ' .. correct_matches)
+print('Accuracy = ' .. accuracy)
