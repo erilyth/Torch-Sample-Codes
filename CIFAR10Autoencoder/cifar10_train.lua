0babc3a8a43a1c450c98c38bb5c81bfd24e481d8
Erilyth <vishal.batchu@students.iiit.ac.in>
2016-06-17 23:51:50 +0530

    Add untrained network and updated network code

--- CIFAR10Autoencoder/cifar10_train.lua
+++ CIFAR10Autoencoder/cifar10_train.lua
@@ -11,26 +11,72 @@ require "image"
 
 classes = {'airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'}
 
-i = 0
-data_subset = torch.load('data_batch_' .. (i+1) .. '.t7', 'ascii')
-image_data = data_subset.data:t()
-image_labels = data_subset.labels
+-- Parametrs for SpatialConvolution = (inputlayers, outputlayers, kernel_width, kernel_height, x_stride, y_stride, x_padding, y_padding)
+-- Parametrs for SpatialMaxPooling = (width, height, x_stride, y_stride, x_padding, y_padding)
+-- Input = 3*32*32 image, Output = 10 sized vector
+cnn = nn.Sequential()
+cnn:add(nn.SpatialConvolution(3,64,5,5,1,1,2,2))
+cnn:add(nn.Tanh())
+cnn:add(nn.SpatialMaxPooling(2,2,2,2))
+cnn:add(nn.Dropout(0.3))
+cnn:add(nn.SpatialConvolution(64,256,3,3,1,1,1,1))
+cnn:add(nn.Tanh())
+cnn:add(nn.Dropout(0.15))
+cnn:add(nn.SpatialConvolution(256,32,3,3,1,1,1,1))
+cnn:add(nn.Tanh())
+cnn:add(nn.Reshape(32*16*16))
+cnn:add(nn.Linear(32*16*16,128))
+cnn:add(nn.Tanh())
+cnn:add(nn.Linear(128,10))
+cnn:add(nn.Tanh())
 
-idx = 1
-cur_image = image_data[idx]
-cur_r = cur_image[{{1,1024}}]
-cur_g = cur_image[{{1025,2048}}]
-cur_b = cur_image[{{2049,3072}}]
+criterion = nn.MSECriterion()
 
--- Generate an image of size 3*32*32
--- First is the color channel, second is the height and last is the width
-fin_image = torch.Tensor(3,32,32)
-for j=1,32 do
-	for k=1,32 do
-		fin_image[1][j][k] = cur_r[(j-1)*32+k]
-		fin_image[2][j][k] = cur_g[(j-1)*32+k]
-		fin_image[3][j][k] = cur_b[(j-1)*32+k]
+-- Run the training 'iterations' number of times
+iterations = 5
+
+for tt=1,iterations do
+    print('Epoch = ' .. tt)
+	for iti=0,4 do
+		data_subset = torch.load('data_batch_' .. (iti+1) .. '.t7', 'ascii')
+		image_data = data_subset.data:t()
+		image_labels = data_subset.labels[1]
+		no_of_training_cases = 10000
+	    for p=1,no_of_training_cases do
+		    local cur_image = image_data[p]
+			local cur_r = cur_image[{{1,1024}}]
+			local cur_g = cur_image[{{1025,2048}}]
+			local cur_b = cur_image[{{2049,3072}}]
+			-- Generate an image of size 3*32*32
+			-- First is the color channel, second is the height and last is the width
+			local input = torch.Tensor(3,32,32)
+			for j=1,32 do
+				for k=1,32 do
+					input[1][j][k] = cur_r[(j-1)*32+k]
+					input[2][j][k] = cur_g[(j-1)*32+k]
+					input[3][j][k] = cur_b[(j-1)*32+k]
+				end
+			end
+		    local output_val = image_labels[p]
+	        local output = torch.Tensor(10);
+	        input = input:double()
+	   	    input = input / 255.0
+	        for k=1,10 do
+	            output[k] = 0.0
+	        end
+	        output[output_val+1] = 1.0
+	        -- Forward prop in the neural network
+	        criterion:forward(cnn:forward(input), output)
+	        -- Reset gradient accumulation
+	        cnn:zeroGradParameters()
+	        -- Accumulate gradients and back propogate
+	        cnn:backward(input, criterion:backward(cnn.output, output))
+	        -- Update with a learning rate
+	        cnn:updateParameters(0.03)
+	       	print(p .. " => " .. output_val)
+	    end
 	end
 end
 
-print(fin_image)
+print('Done training, saving model if needed')
+torch.save('model.torch', cnn)
